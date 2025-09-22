df0cf0db7cc056998ad1566478cd3407d8359d40
김정덕 <lemon2ea@gmail.com>
2025-09-22 12:58:38 +0900

    Update constants.tsx

--- constants.tsx
+++ constants.tsx
@@ -25,13 +25,13 @@ export const WEB_APPS: WebApp[] = [
     ),
   },
   {
-    id: 'cloud-storage',
-    title: 'Drive',
-    description: 'Cloud file storage',
-    url: 'https://drive.google.com',
+    id: 'suno AI',
+    title: 'AI Music',
+    description: 'AI Music',
+    url: 'https://suno.com/create',
     icon: (
         <svg xmlns="http://www.w3.org/2000/svg" className="h-16 w-16 text-green-400" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={1.5}>
-          <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 12.75V12A2.25 2.25 0 014.5 9.75h15A2.25 2.25 0 0121.75 12v.75m-8.69-6.44l-2.12-2.12a1.5 1.5 0 00-1.061-.44H4.5A2.25 2.25 0 002.25 6v12a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18V9a2.25 2.25 0 00-2.25-2.25h-5.379a1.5 1.5 0 01-1.06-.44z" />
+          <path strokeLinecap="round" strokeLinejoin="round" d="M9 9l10-5m-10 5v12l10-5M9 9l10 5m-10-5l-3 2" />
         </svg>
     ),
   },
